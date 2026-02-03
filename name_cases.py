318893986c550eb8466f4205c5817422ba1cdce0
joiemassey <joiemassey47@gmail.com>
2026-02-03 13:20:56 -0500

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,17 @@
+# Joie Massey
+
+first_name = "sheila"
+last_name = "west"
+full_name = f"{first_name} {last_name}"
+print(full_name.lower())
+
+first_name = "sheila"
+last_name = "west"
+full_name = f"{first_name} {last_name}"
+print(full_name.upper())
+
+first_name = "sheila"
+last_name = "west"
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
+
