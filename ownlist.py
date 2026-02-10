3021bc90231c65800d5c41b8dd8496c49e6ada4e
joiemassey <joiemassey47@gmail.com>
2026-02-10 18:27:25 -0500

    Add files via upload

---
+++ ownlist.py
@@ -0,0 +1,9 @@
+transportation = ['car', 'motorcycle', 'plane']
+message = f"I would like to own a {transportation[0]}"
+print(message)
+
+message = f"I wonder if I have what it takes to have a {transportation[1]} for a daily ride?"
+print(message)
+
+message = f"I prefer taking a {transportation[2]} for longer trips."
+print(message)
