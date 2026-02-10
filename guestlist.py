2acfe6ce71adee8a5a5cea5f4014e567e567a740
joiemassey <joiemassey47@gmail.com>
2026-02-10 18:39:34 -0500

    Add files via upload

---
+++ guestlist.py
@@ -0,0 +1,10 @@
+guests = ['amy winehouse', 'bad bunny', 'andre 3000']
+message = f"{guests[0].title()} would you like to come over for dinner?"
+print(message)
+
+message = f"I wonder if {guests[1].title()} would be willing to stop by for dinner after the half-time show?"
+print(message)
+
+message = f"{guests[2].title()}, we are having an Outkast themed dinner party and would love for you to come."
+print(message)
+
