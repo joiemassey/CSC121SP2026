ce26bc866b6048ec82c903ac3b38bcd9e2f890fe
joiemassey <joiemassey47@gmail.com>
2026-02-10 19:02:47 -0500

    Add files via upload

---
+++ guestlist_sorted.py
@@ -0,0 +1,43 @@
+guests = ['amy winehouse', 'bad bunny', 'andre 3000']
+message = f"{guests[0].title()} would you like to come over for dinner?"
+print(message)
+
+message = f"I wonder if {guests[1].title()} would be willing to stop by for dinner after the half-time show?"
+print(message)
+
+message = f"{guests[2].title()}, we are having an Outkast themed dinner party and would love for you to come."
+print(message)
+
+message = "I found a bigger table. I will invite more people." 
+print(message)
+
+guests = ['amy winehouse', 'bad bunny', 'andre 3000']
+guests.insert(0, 'sza')
+print(guests)
+
+guests.insert(3, 'kendrick lamar')
+print(guests)
+
+guests.append('griz')
+print(guests)
+
+guests.sort()
+print(guests)
+
+message = f"{guests[0].title()} would you like to come over for dinner?"
+print(message)
+
+message = f"{guests[1].title()}, we are having an Outkast themed dinner party and would love for you to come."
+print(message)
+
+message = f"I wonder if {guests[2].title()} would be willing to stop by for dinner after the half-time show?"
+print(message)
+
+message = f"{guests[3].title()}, I know you just went back on tour, but I made chicken and dumplings. Please stop by."
+print(message)
+
+message = f"I love your music, {guests[4].title()}. You are invited to our dinner party."
+print(message)
+
+message = f"We need a beautiful vocalist in attendance at dinner. Let's invite {guests[5].title()}."
+print(message)
